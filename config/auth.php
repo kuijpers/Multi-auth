a6c3a068d0221691912e89604a03f61f0e47b810
kuijpers <darkknightworks@gmail.com>
2017-04-24 15:24:46 +0200

    Added password_reset options

--- config/auth.php
+++ config/auth.php
@@ -112,6 +112,7 @@ return [
             'table' => 'password_resets',
             'expire' => 60,
         ],
+		'admins' => [
 			'provider' => 'admins',
 			'table' => 'password_resets',
 			'expire' => 60,
