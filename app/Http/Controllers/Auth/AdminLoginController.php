a6e3d652dc7ca70b2d9b3f2c196e29d7adb25623
kuijpers <darkknightworks@gmail.com>
2017-05-01 13:12:17 +0200

    Created the login method

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class AdminLoginController extends Controller
 {
@@ -16,4 +17,31 @@ class AdminLoginController extends Controller
 	public function showLoginForm(){
     	return view('auth.admin-login');
 	}
+
+	public function login(Request $request){
+
+	// Validate the form data
+	$this->validate($request, [
+		'email'	=> 'required|email',
+		'password'	=> 'required',
+	]);
+
+	// Attempt to log the user in
+
+	// Set the credentials
+
+	$credentials = [
+		'email' => $request->email,
+		'password' => $request->password,
+	];
+
+	if(Auth::guard('admin')->attempt($credentials, $request->remember)) {
+
+		// If successful , then redirect to their intended location
+		return redirect()->intended(route('admin.dashboard'));
+
+	}
+	// If unsuccessful , then redirect back to the login with the form data
+	return redirect()->back()->withInput($request->only('email', 'remember'));
+}
 }
