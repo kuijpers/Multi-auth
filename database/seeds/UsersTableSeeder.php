f1672104a983a60edd14ace0bb6ba0c478680895
kuijpers <darkknightworks@gmail.com>
2017-05-01 10:51:49 +0200

    Created seeders to add data to the DB so we can use it in our project.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		DB::table('users')->insert([
+			'name'        	=> 'Dennis Kuijpers',
+			'email'         => 'dennis@test.nl',
+			'password'      => bcrypt('welkom'),
+			'created_at'	=> date('Y-m-d H:i:s'),
+			'updated_at'    => date('Y-m-d H:i:s'),
+		]);
+
+    }
+}
